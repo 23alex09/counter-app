b417380d65140885359ce5f2654cd999cbb794eb
Alejandro Hidalgo Acuña <23alex09@gmail.com>
2022-12-14 15:15:28 +0100

    implements substraction and reset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -5,15 +5,24 @@ export const CounterApp = ( { value } ) => {
 
     const [ counter, setCounter ] = useState( value );
 
-    const handleAdd = () => {
-        setCounter( ( currentCounter ) => currentCounter + 1 );
-    }
+    const handleAdd = () =>
+        setCounter( counter + 1 );
+
+
+    const handleSubstract = () =>
+        setCounter( counter - 1 );
+
+
+    const handleReset = () => setCounter( 0 );
+
 
     return (
         <>
             <h1>Counter App</h1>
             <h2>{ counter }</h2>
             <button onClick={ handleAdd }> +1 </button>
+            <button onClick={ handleSubstract }> -1 </button>
+            <button onClick={ handleReset }> Reset </button>
         </>
     )
 }
