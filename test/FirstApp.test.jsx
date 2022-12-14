065b64ee1d998c62c781153b104143e9361c98cf
Alejandro Hidalgo Acuña <23alex09@gmail.com>
2022-12-14 17:46:21 +0100

    refactor FirstApp test suit

--- test/FirstApp.test.jsx
+++ test/FirstApp.test.jsx
@@ -1,35 +1,33 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { FirstApp } from "../src/FirstApp"
 
 describe( 'test suit for FirstApp component', () => {
-    // test( 'should match with the snapshot', () => {
 
-    //     const title = 'Hello, World!';
-    //     const { container } = render( <FirstApp title={ title } /> );
+    const title = 'Hello, World!';
+    const subtitle = 123;
 
-    //     expect( container ).toMatchSnapshot();
-    // } )
+    test( 'should match with the snapshot', () => {
+        const { container } = render( <FirstApp title={ title } /> )
+        expect( container ).toMatchSnapshot();
+    } )
+
+
+    test( 'should show the title Hello, World!', () => {
+        render( <FirstApp title={ title } /> );
+        expect( screen.getByText( title ) ).toBeTruthy();
+    } )
 
     test( 'should show the title within a h1', () => {
 
-        const title = 'Hello, World!';
-        const { container, getByText, getByTestId } = render( <FirstApp title={ title } /> );
-        expect( getByText( title ) ).toBeTruthy();
+        render( <FirstApp title={ title } /> );
+        expect( screen.getByRole( 'heading', { level: 1 } ).innerHTML ).toContain( title );
 
-        // const h1 = container.querySelector( 'h1' );
-        // expect( h1.innerHTML ).toContain( title );
-
-        expect( getByTestId( 'test-title' ).innerHTML ).toContain( title );
     } )
 
     test( 'should show the subtitle send by props', () => {
-        const title = 'Hello, World!';
-        const subtitle = 123;
-        const { getByText } = render( <FirstApp
-            title={ title }
-            subtitle={ subtitle }
-        /> );
-        expect( getByText( subtitle ) ).toBeTruthy();
+
+        render( <FirstApp title={ title } subtitle={ subtitle } /> )
+        expect( screen.getByText( subtitle ) ).toBeTruthy();
 
     } )
 } )
