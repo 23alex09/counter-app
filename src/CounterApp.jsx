e4c40901417fcb8b87d1defb0b979346a3c1faa6
Alejandro Hidalgo Acuña <23alex09@gmail.com>
2022-12-14 14:57:42 +0100

    add funtionality to CounterApp using useState hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types';
 
-export const CounterApp = ({ value }) => {
+export const CounterApp = ( { value } ) => {
+
+    const [ counter, setCounter ] = useState( value );
+
+    const handleAdd = () => {
+        setCounter( ( currentCounter ) => currentCounter + 1 );
+    }
+
     return (
         <>
             <h1>Counter App</h1>
-            <h2>{value}</h2>
+            <h2>{ counter }</h2>
+            <button onClick={ handleAdd }> +1 </button>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CounterApp } from './CounterApp';
-import { FirstApp } from './FirstApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <CounterApp value={123} />
+        <CounterApp value={0} />
     </React.StrictMode>
 )
